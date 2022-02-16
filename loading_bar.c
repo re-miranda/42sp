1a87cf1977ba9e1aa5cec21d663f425487230daf
re-miranda <rmiranda@student.42sp.org.br>
2022-02-16 05:10:24 +0100

    faltou header

--- loading_bar.c
+++ loading_bar.c
@@ -6,11 +6,12 @@
 /*   By: rmiranda <rmiranda@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/04 05:36:30 by rmiranda          #+#    #+#             */
-/*   Updated: 2022/02/09 08:33:55 by rmiranda         ###   ########.fr       */
+/*   Updated: 2022/02/16 05:05:46 by rmiranda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <unistd.h>
 
 int	main(void)
 {
