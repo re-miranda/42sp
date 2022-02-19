9ba1423fb092bdab56263bedbce8be4d6e4bebe7
re-miranda <rmiranda@student.42sp.org.br>
2022-02-19 14:01:51 +0100

    last push in 42 basecamp

---
+++ all/testes_template.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   testes_template.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmiranda <rmiranda@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/16 04:32:20 by rmiranda          #+#    #+#             */
+/*   Updated: 2022/02/16 05:14:20 by rmiranda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// Testes criados por "rmiranda | Renato"
+// Os testes são todos feitos com printf();
+// Esse é apenas um método de testar
+// Sinta-se livre para modificar e implementar sua própria maneira
+// Aliases de execução de dentro da pasta do exercício
+// | | |
+// V V V
+alias norm='clear && norminette -R CheckForbiddenSourceHeader'
+alias run='clear && gcc -Wextra -Werror -Wall *.c -o ~/teste.out && ~/teste.out'
+alias run2='clear && gcc *.c -o ~/teste.out && ~/teste.out'
+
+// ==================== EXERCÍCIO 00 ====================
